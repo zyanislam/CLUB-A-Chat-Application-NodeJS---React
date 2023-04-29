ac7c1e548ee4410bc787d0a146ac382a9c596ab0
Zyan <fislam183070@bscse.uiu.ac.bd>
2023-04-29 21:40:20 +0600

    Backend REST

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require("express"); // Run HTTP server
+const cors = require("cors"); // Run CORS server anyway
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  return res.json({ username: username, secret: "sha256..." });
+});
+
+app.listen(3001); // Port 3001
