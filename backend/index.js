37126320fd8f223c1cdaed3cec265ffc8a3ec8ae
Zyan <fislam183070@bscse.uiu.ac.bd>
2023-04-29 21:45:40 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,9 @@ app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
-  const { username } = req.body;
+    const { username } = req.body;
+    //eb2f4381-3650-42bf-bc1f-5907a567bdcf
+
   return res.json({ username: username, secret: "sha256..." });
 });
 
